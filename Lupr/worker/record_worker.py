ee826d40c0fdb604f2c4f1ba548fbad1ee2ade64
azzamsa <azzam@azzamsa.com>
2019-05-08 11:35:30 +0700

    Rename `_main_ctrl` to `_controller`

--- Lupr/worker/record_worker.py
+++ Lupr/worker/record_worker.py
@@ -9,16 +9,16 @@ class RecordWorker(QThread):
     def __init__(self, model, controller):
         QThread.__init__(self)
         self._model = model
-        self._main_ctrl = controller
+        self._controller = controller
 
     def __del__(self):
         self.wait()
 
     def run(self):
         while True:
-            auth_info = self._main_ctrl.get_auth_info()
-            all_windows = self._main_ctrl.get_all_windows()
-            focused_window = self._main_ctrl.get_focused_window()
+            auth_info = self._controller.get_auth_info()
+            all_windows = self._controller.get_all_windows()
+            focused_window = self._controller.get_focused_window()
             self._model.write_auth_info(auth_info)
             self._model.write_all_windows(all_windows)
             self._model.write_focused_window(focused_window)
